a75401a1731133802fcd4397923b3d2f02d20ee8
Tiago <tiagohs.dev@gmail.com>
2022-07-09 21:41:28 -0300

    11: define os middlewares de validação com joi

---
+++ src/middlewares/index.ts
@@ -0,0 +1,68 @@
+import { ErrorRequestHandler, RequestHandler } from 'express';
+import joi from 'joi';
+import { IUserCreateRequest, IUserUpdateRequest } from '../interfaces';
+import { BadRequest } from '../utils/errors';
+import HttpError from '../utils/errors/HttpError';
+
+export default class Middlewares {
+  private static _createSchema = joi.object({
+    firstName: joi.string().required(),
+    lastName: joi.string().required(),
+    email: joi.string().required(),
+    occupation: joi.string().required(),
+  });
+
+  private static _updateSchema = joi.object({
+    firstName: joi.string(),
+    lastName: joi.string(),
+    email: joi.string(),
+    occupation: joi.string(),
+  });
+
+  public static error: ErrorRequestHandler = (err, _req, res, _next) => {
+    if (err instanceof HttpError) {
+      const { httpCode, message } = err;
+      return res.status(httpCode).json({ error: { message } });
+    }
+
+    return res.status(500).json({ error: { message: err.message } });
+  };
+
+  public static createValidation: RequestHandler = (req, _res, next) => {
+    const {
+      email,
+      firstName,
+      lastName,
+      occupation
+    } = req.body as IUserCreateRequest;
+    const { error } = Middlewares._createSchema.validate({
+      email,
+      firstName,
+      lastName,
+      occupation,
+    });
+
+    if (error) return next(new BadRequest(error.message));
+
+    next();
+  };
+
+  public static _updateValidation: RequestHandler = (req, _res, next) => {
+    const {
+      email,
+      firstName,
+      lastName,
+      occupation
+    } = req.body as IUserUpdateRequest;
+    const { error } = Middlewares._updateSchema.validate({
+      email,
+      firstName,
+      lastName,
+      occupation,
+    });
+
+    if (error) return next(new BadRequest(error.message));
+
+    next();
+  }
+}
